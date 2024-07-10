de97182029c6c3c7c13fc7d29fd817ed044fd9fc
Mahmud Hasan Siddique <mahmudhasan.mm07@gmail.com>
2024-07-10 17:46:00 +0600

    Update indexb1.js

---
+++ indexb1.js
@@ -0,0 +1,47 @@
+const array = [1,2,3,4,5,6,7,8,9,10]
+// const array = [1,7,3,6,5,4,2,10,9,8,13,12,18,15,16,28,24,21,29,22]
+const names = ["mahmud", "nitol", "Masud", "noman"]
+const name = 'Mahmud Hasan Siddque'
+const obj = {
+    name : "Mehadi Hasan",
+    age : 22,
+    gender : "Male"
+}
+
+// for(let i =0; i< array.length; i++){
+//     if(i<5){
+//         console.log(array[i]);
+
+//     }
+// }
+
+// array.map(e=> console.log(e))
+
+// const newArray = array.filter(e=> e%2 ==1)
+
+//  const newArray =array.slice(0,5)
+// array.splice(5,3,15)
+
+const newArray = array.find((e)=> e==50)
+
+
+const newName= name.split(" ")
+const array2 = [11,12,13,14,15]
+
+// const concatArray = array.concat(array2)
+// const includeArray = array.includes(5)
+// const includeArray = array.map(e=> e.includes(5))
+// const concatArray = array.reduce((p,c)=> p+c,10)
+// const concatArray = names.sort()
+
+// obj.name = "ilytsi Zeraw Senpai"
+// array.push(11)
+// array.pop()
+
+const number = "5"
+const newNumber = parseInt(number)
+
+const guessNumber = Math.round(5.8)
+
+const seqNumber= [1,2,3,4]
+console.log(guessNumber);
